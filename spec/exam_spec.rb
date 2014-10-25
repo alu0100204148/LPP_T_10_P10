96275cb5413e8500d6333d14ea2de251dee3465a
alu0100204148 <alu0100204148@ull.edu.es>
2014-10-25 16:07:51 +0000

    exam_spec.rb editado

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -1,41 +1,42 @@
-require "./lib/exam.rb"
+# -*- coding: utf-8 -*-
+require './lib/exam.rb'
 
 describe Exam::Examen do
- # exam = Exam::Examen.new
   before :each do
-    @ex = Exam::Examen.new
-
+    @examen = Exam::Examen.new("¿De que color es el caballo blanco de santiago?" , [ 'a) Rojo', 'b) Blanco', 'c) Azul' ])
   end
 
-  describe "# existencia de pregunta" do
+  describe "# Existencia de pregunta" do
     it "Existe una pregunta" do
-      expect(@ex.preguntas.length).to be >= 1
+      expect(@examen.pregunta).not_to be_empty
+      
     end
   end
 
-  describe "# existencia de respuestas" do
+  describe "# Existencia de respuestas" do
     it "Existe mas de una respuesta" do
-      expect(@ex.respuestas.length).to be >= 2
+      expect(@examen.numero).to be > 1
     end
   end
 
-  describe "# obtener pregunta" do
+  describe "# Obtener pregunta" do
     it "Se obtiene una pregunta" do
-      @ex.obtenerPregunta
+      expect(@examen.pregunta).to match(/¿*^?/)
+      
     end
   end
 
-  describe "# obtener respuestas" do
+  describe "# Obtener respuestas" do
     it "Se obtienen las respuestas" do
-      @ex.obtenerRespuestas
+      expect(@examen.obtenerRespuestas(0)).to match(/a\) /)
+      expect(@examen.obtenerRespuestas(1)).to match(/b\) /)
+      expect(@examen.obtenerRespuestas(2)).to match(/c\) /)
     end
   end
 
-  describe "# mostrar resultado" do
+  describe "# Mostrar resultado" do
     it "Se muestra una pregunta seguida de sus respuestas" do
-      @ex.obtenerPregunta
-      @ex.obtenerRespuestas
-      @ex.mostrar
+      expect(@examen.mostrar).to match(/\?*\?*a\)*b\)*c*\)*/) 
     end
   end
 
