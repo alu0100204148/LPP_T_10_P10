6965697f1234225541f3859c74a2bf7e7d805a81
alu0100204148 <alu0100204148@ull.edu.es>
2014-10-25 16:10:08 +0000

    exam.rb editado

--- lib/exam.rb
+++ lib/exam.rb
@@ -1,65 +1,43 @@
 # -*- coding: utf-8 -*-
-require 'rubygems'
-require 'bundler/setup'
-require 'rspec'
-require 'exam/version'
+require './lib/exam.rb'
 
-module Exam
-  class Examen
-    attr_reader :preguntas, :respuestas, :n, :pr, :nresp, :re
-    def initialize
-      @n = 0
-      @pr = ""
-      @re = Array.new
-      @nresp = 4
-      pregunta1 = "¿Cual es el resultado de sumar 2 y 5?"
-      pregunta2 = "¿De que color es el caballo blanco de Santiago?"
-      pregunta3 = "¿Cual es el simbolo de la raiz cuadrada?"
-      @preguntas = [pregunta1, pregunta2, pregunta3]
-      respuesta11 = "a) 4"
-      respuesta12 = "b) 5"
-      respuesta13 = "c) 6"
-      respuesta14 = "d) Ninguna de las anteriores"
-      respuesta21 = "a) Azul"
-      respuesta22 = "b) Blanco"
-      respuesta23 = "c) Rayas blancas y negras"
-      respuesta24 = "d) No se que es un caballo"
-      respuesta31 = "a) ^_^"
-      respuesta32 = "b) %"
-      respuesta33 = "c) &"
-      respuesta34 = "d) Ninguno de los anteriores"
-      @respuestas = [respuesta11, respuesta12, respuesta13, respuesta14, respuesta21, respuesta22, respuesta23, respuesta24, respuesta31, respuesta32, respuesta33, respuesta34]
-    end
-
-
-    def obtenerPregunta
-      @n = rand(@preguntas.length)
-      @pr = @preguntas[@n]
-    end
-
-
-    def obtenerRespuestas
-      for i in 0..@nresp-1
-        @re[i] = @respuestas[(@n*@nresp)+i]
-      end
-    end
-
-
-    def mostrar(frase)
-      puts frase
-    end
-
-
-    def mostrar
-      puts @pr
-      @re.each do |resp|
-        puts resp
-      end
-    end
-
+describe Exam::Examen do
+  before :each do
+    @examen = Exam::Examen.new("¿De que color es el caballo blanco de santiago?" , [ 'a) Rojo', 'b) Blanco', 'c) Azul' ])
   end
+
+  describe "# Existencia de pregunta" do
+    it "Existe una pregunta" do
+      expect(@examen.pregunta).not_to be_empty
+      
+    end
+  end
+
+  describe "# Existencia de respuestas" do
+    it "Existe mas de una respuesta" do
+      expect(@examen.numero).to be > 1
+    end
+  end
+
+  describe "# Obtener pregunta" do
+    it "Se obtiene una pregunta" do
+      expect(@examen.pregunta).to match(/¿*^?/)
+      
+    end
+  end
+
+  describe "# Obtener respuestas" do
+    it "Se obtienen las respuestas" do
+      expect(@examen.obtenerRespuestas(0)).to match(/a\) /)
+      expect(@examen.obtenerRespuestas(1)).to match(/b\) /)
+      expect(@examen.obtenerRespuestas(2)).to match(/c\) /)
+    end
+  end
+
+  describe "# Mostrar resultado" do
+    it "Se muestra una pregunta seguida de sus respuestas" do
+      expect(@examen.mostrar).to match(/\?*\?*a\)*b\)*c*\)*/) 
+    end
+  end
+
 end
-
-
-
-
