9166185caf136ecc324ff65f3911f29cde3583cd
Alvaro Suarez <alu0100315462@ull.edu.es>
2014-10-31 00:47:34 +0000

    Creo los metodos necesarios en la clase List para que pasen las pruebas

--- lib/list.rb
+++ lib/list.rb
@@ -8,9 +8,25 @@ Node = Struct.new(:value, :next)
 
 module Exam
   class List
+    attr_reader :cabeza
 
+    def initialize
+      @cabeza = nil
+    end
+
+    def push(*valor)
+      for i in 0...valor.length
+        nuevoNode = Node.new(valor[i], nil)
+        nuevoNode.next = @cabeza
+        @cabeza = nuevoNode
+      end
+    end
+
+
+    def pop
+      @cabeza = @cabeza.next
+    end
 
   end
 end
-
 
