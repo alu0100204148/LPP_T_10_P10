5b851bea3c1f7352e27371ac73cbd897da94800c
Alvaro Suarez <alu0100315462@ull.edu.es>
2014-10-31 00:45:15 +0000

    Creo el fichero para la clase List, todavia sin hacer, y que hara uso de un Struct

---
+++ lib/list.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'bundler/setup'
+require 'rspec'
+
+# create a Struct with :value and :next
+Node = Struct.new(:value, :next)
+
+module Exam
+  class List
+
+
+  end
+end
+
+
