889dbcac80dbb3ed8d4abc9ab391993dc4708e5e
alu0100204148 <alu0100204148@ull.edu.es>
2014-11-25 01:44:44 +0000

    Simplificacion Funciones

--- lib/inversor/inversor.rb
+++ lib/inversor/inversor.rb
@@ -1,28 +1,11 @@
-
-    
-    
 module Inversor
-
-    def invertirLista2(lista)
-        listaTemp = Exam::List.new
-        while lista.numeroNodos != 0 do
-            valor = lista.sacarValor
-            listaTemp.push(valor)
-        end
-        return listaTemp; 
-    end
-    
     
     def invertirLista(lista)
         listaTemp = Exam::List.new
         lista.each { |elemento| listaTemp.push(elemento) }
         return listaTemp
     end
-    
-    
-    
-    
-    
+
     def invertirExamen(examen)
         preguntas = examen.obtenerPreguntas
         soluciones = examen.obtenerSoluciones
