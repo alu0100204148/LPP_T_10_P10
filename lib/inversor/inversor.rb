8e19996e3b910beeefa80c923383b6dd6de81356
alu0100315462 <alu0100315462@ull.edu.es>
2014-11-27 15:33:00 +0000

    Comentarios en el fichero inversor.rb para el modulo Inversor

--- lib/inversor/inversor.rb
+++ lib/inversor/inversor.rb
@@ -1,11 +1,16 @@
+#Modulo que contiene las funciones que se ocupan de invertir listas de un examen.
 module Inversor
     
+    #Funcion que recibe una lista (de preguntas o respuestas) y devuelve otra con
+    #los elementos ordenados de manera inversa.
     def invertirLista(lista)
         listaTemp = Exam::List.new
         lista.map { |elemento| listaTemp.push(elemento) }
         return listaTemp
     end
 
+    #Funcion que recibe un examen y llama a la funcion invertirLista tanto para las
+    #preguntas como para las respuestas del examen recibido.
     def invertirExamen(examen)
         examen.establecerPreguntas (invertirLista (examen.obtenerPreguntas))
         examen.establecerSoluciones (invertirLista (examen.obtenerSoluciones))
@@ -14,4 +19,5 @@ module Inversor
 
 end
 
+#Se incluye el modulo Inversor en la clase UI
 class Exam::UI; include Inversor; end
